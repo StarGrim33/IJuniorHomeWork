369209fa71f169272631c2b8be68e849541ecc66
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-06-30 23:05:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ijunior1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            int phoneNumber;
+            float waistGirth;
+            byte height;
+            char sex;
+            bool isOpen;
+            string name;
+            double seconds;
+            bool isEnoughMoney;
+            int score;
+            string work;
+        }
+    }
+}
