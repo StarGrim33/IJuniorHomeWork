9ab342dcad68f422d35c147366299425b2fbb765
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-01 09:56:44 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,12 +4,16 @@ namespace JuniorHomeWork
     {
         static void Main(string[] args)
         {
-            string firstNameOfUser = "Kuzmichev";
-            string secondNameOfUser = "Vladislav";
-            Console.WriteLine($"Ваше имя: {firstNameOfUser}. Фамилия: {secondNameOfUser}");
-            Console.WriteLine("Упс, что то пошло не так, нажмите любую клавишу, чтобы все исправить!");
-            (firstNameOfUser, secondNameOfUser) = (secondNameOfUser, firstNameOfUser);
-            Console.WriteLine($"Ваше имя: {firstNameOfUser}. Фамилия: {secondNameOfUser}");
+            int picturesInRow = 3;
+            int picturesInAlbum = 52;
+            Console.WriteLine($"Картинок в ряд: {picturesInRow}, всего картинок в альбоме: {picturesInAlbum}");
+            Console.WriteLine("Полностью заполненных рядов: ");
+            int filledRow = picturesInAlbum / picturesInRow;
+            Console.WriteLine(filledRow);
+            Console.WriteLine("Картинок сверх меры: ");
+            int notEnoughFilledRow = picturesInAlbum % picturesInRow;
+            Console.WriteLine(notEnoughFilledRow);
+            Console.ReadLine();
         }
     }
 }
