41081a2655119f0b25966a06494d0751633a5ec9
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-01 09:13:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,19 +4,12 @@ namespace JuniorHomeWork
     {
         static void Main(string[] args)
         {
-            string nameOfUser;
-            int ageOfUser;
-            string zodiacSign;
-            string workOfUser;
-            Console.WriteLine("Здравствуйте! Как Вас зовут? : ");
-            nameOfUser = Console.ReadLine();
-            Console.WriteLine("Сколько Вам лет? : ");
-            ageOfUser = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Какой у Вас знак зодиака? : ");
-            zodiacSign = Console.ReadLine();
-            Console.WriteLine("Кем вы работаете? : ");
-            workOfUser = Console.ReadLine();
-            Console.WriteLine($"Ваше имя {nameOfUser}, Вам {ageOfUser} лет, Ваш знак зодиака - {zodiacSign}, Вы работаете {workOfUser}");
+            string firstNameOfUser = "Kuzmichev";
+            string secondNameOfUser = "Vladislav";
+            Console.WriteLine($"Ваше имя: {firstNameOfUser}. Фамилия: {secondNameOfUser}");
+            Console.WriteLine("Упс, что то пошло не так, нажмите любую клавишу, чтобы все исправить!");
+            (firstNameOfUser, secondNameOfUser) = (secondNameOfUser, firstNameOfUser);
+            Console.WriteLine($"Ваше имя: {firstNameOfUser}. Фамилия: {secondNameOfUser}");
         }
     }
 }
