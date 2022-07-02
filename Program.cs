14c78ed346e8eb05554312e4180cff4b8fb880db
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-02 20:05:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace JuniorHomeWork
         {
             string stopWord;
 
-            while (true)
+            for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine("Напишите стоп слово - quit");
                 stopWord = Console.ReadLine();
 
                 if (stopWord == "quit")
                 {
-                    break;
+                    i = 100;
                 }
             }
             Console.WriteLine("Цикл остановлен!");
