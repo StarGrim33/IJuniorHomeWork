5837b5142ce6f466e7a8d5343f170b75b18852f1
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-02 14:06:14 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,42 +5,39 @@ namespace JuniorHomeWork
         static void Main(string[] args)
         {
             int moneyOfUser;
-            int priceOfOneCrystall = 5;
+            int crystalPrice = 5;
             string approvalOfDeal = "да";
             int countOfUserCrystalls;
-            Console.WriteLine($"Приветствуем Вас в нашем магазине кристаллов, один кристалл стоит {priceOfOneCrystall} серебренников, " +
+            int crystallsBying = 15;
+            Console.WriteLine($"Приветствуем Вас в нашем магазине кристаллов, один кристалл стоит {crystalPrice} серебренников, " +
                 $"сколько у Вас золотых ? : ");
             moneyOfUser = Convert.ToInt32(Console.ReadLine());
 
-            if (moneyOfUser >= priceOfOneCrystall)
+            if (moneyOfUser >= crystalPrice)
             {
-                Console.WriteLine($"Предлагаем Вам приобрести {moneyOfUser} кристаллов, Вы согласны? Напишите да или нет : ");
+                Console.WriteLine($"Предлагаем Вам приобрести {crystallsBying} кристаллов, Вы согласны? Напишите да или нет : ");
                 approvalOfDeal = Console.ReadLine();
-
                 if (approvalOfDeal == "да")
                 {
-                    countOfUserCrystalls = moneyOfUser;
-                    Console.WriteLine($"Вы купили {moneyOfUser} кристаллов");
+                    countOfUserCrystalls = crystallsBying;
+                    moneyOfUser -= crystallsBying * crystalPrice;
+                    Console.WriteLine($"Вы купили {crystallsBying} кристаллов, у Вас осталось {moneyOfUser} кристаллов");
                 }
-
-                else
+                if (approvalOfDeal == "нет")
                 {
                     Console.WriteLine("Какое Ваше предложение? Сколько кристаллов Вы хотите купить?: ");
-                    countOfUserCrystalls = Convert.ToInt32(Console.ReadLine());
-                    
-                    if (countOfUserCrystalls <= moneyOfUser)
+                    crystallsBying = Convert.ToInt32(Console.ReadLine());
+                    if (crystallsBying <= moneyOfUser)
                     {
-                        moneyOfUser -= priceOfOneCrystall * countOfUserCrystalls;
-                        Console.WriteLine($"Вы приобрели {countOfUserCrystalls} кристаллов, у Вас осталось {moneyOfUser} серебренников");
+                        moneyOfUser -= crystalPrice * crystallsBying;
+                        Console.WriteLine($"Вы приобрели {crystallsBying} кристаллов, у Вас осталось {moneyOfUser} серебренников");
                     }
-                    
-                    else if (countOfUserCrystalls > moneyOfUser)
+                    else if (crystallsBying > moneyOfUser)
                     {
-
                         Console.WriteLine("У вас недостаточно денег для покупки");
-                    }
                     }
                 }
             }
         }
     }
+}
