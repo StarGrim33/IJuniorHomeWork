bcf72dc5abac500726ce78503b8dfd442612e22f
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-02 16:06:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,18 +4,17 @@ namespace JuniorHomeWork
     {
         static void Main(string[] args)
         {
-            int moneyOfUser;
-            int crystalPrice = 5;
-            int countOfUserCrystalls;
-            int crystallsBying;
-            Console.WriteLine($"Приветствуем Вас в нашем магазине кристаллов, один кристалл стоит {crystalPrice} серебренников, " +
-                $"сколько у Вас золотых ? : ");
-            moneyOfUser = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Сколько кристаллов Вы хотите приобрести?");
-            crystallsBying = Convert.ToInt32(Console.ReadLine());
-            moneyOfUser -= crystalPrice * crystallsBying;
-            countOfUserCrystalls = crystallsBying;
-            Console.WriteLine($"Теперь у Вас {countOfUserCrystalls} кристаллов и {moneyOfUser} серебренников");
+            int pateintInQueue;
+            int timePerPacient = 10;
+            int estimatedTimeHours;
+            int estimatedTimeMinutes;
+
+            Console.WriteLine("Введите количество старушек в очереди: ");
+            pateintInQueue = Convert.ToInt32(Console.ReadLine());
+            estimatedTimeHours = (timePerPacient * pateintInQueue) / 60;
+            estimatedTimeMinutes = (timePerPacient * pateintInQueue) % 60;
+
+            Console.WriteLine($"Время ожидания в очереди составляет {estimatedTimeHours} часа и {estimatedTimeMinutes} минут");
         }
     }
 }
