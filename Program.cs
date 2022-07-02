84e5fe03dad3fb16d412704d830e49c6e216f34b
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-02 12:53:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,32 +5,40 @@ namespace JuniorHomeWork
         static void Main(string[] args)
         {
             int moneyOfUser;
-            int priceOfOneCristall = 1;
+            int priceOfOneCrystall = 5;
             string approvalOfDeal = "да";
             int countOfUserCrystalls;
-            Console.WriteLine($"Приветствуем Вас в нашем магазине кристаллов, один кристалл стоит {priceOfOneCristall} серебренников, " +
+            Console.WriteLine($"Приветствуем Вас в нашем магазине кристаллов, один кристалл стоит {priceOfOneCrystall} серебренников, " +
                 $"сколько у Вас золотых ? : ");
             moneyOfUser = Convert.ToInt32(Console.ReadLine());
 
-            if (moneyOfUser >= 50)
+            if (moneyOfUser >= priceOfOneCrystall)
             {
                 Console.WriteLine($"Предлагаем Вам приобрести {moneyOfUser} кристаллов, Вы согласны? Напишите да или нет : ");
                 approvalOfDeal = Console.ReadLine();
 
                 if (approvalOfDeal == "да")
                 {
-                    moneyOfUser -= priceOfOneCristall * moneyOfUser;
-                    Console.WriteLine($"Вы купили {moneyOfUser} кристаллов, у Вас осталось {moneyOfUser} серебренников");
+                    countOfUserCrystalls = moneyOfUser;
+                    Console.WriteLine($"Вы купили {moneyOfUser} кристаллов");
                 }
 
-                else 
+                else
                 {
                     Console.WriteLine("Какое Ваше предложение? Сколько кристаллов Вы хотите купить?: ");
                     countOfUserCrystalls = Convert.ToInt32(Console.ReadLine());
-                    moneyOfUser -= priceOfOneCristall * countOfUserCrystalls;
-                    Console.WriteLine($"Вы приобрели {countOfUserCrystalls} кристаллов, у Вас осталось {moneyOfUser} серебренников");
+                    if (countOfUserCrystalls <= moneyOfUser)
+                    {
+                        moneyOfUser -= priceOfOneCrystall * countOfUserCrystalls;
+                        Console.WriteLine($"Вы приобрели {countOfUserCrystalls} кристаллов, у Вас осталось {moneyOfUser} серебренников");
+                    }
+                    else if (countOfUserCrystalls > moneyOfUser)
+                    {
+
+                        Console.WriteLine("У вас недостаточно денег для покупки");
+                    }
+                    }
                 }
-            } 
             }
         }
     }
