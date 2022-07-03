19be8e2c060500875411488c4bef062225af831a
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-03 11:19:40 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,11 +5,13 @@ namespace JuniorHomeWork
         static void Main(string[] args)
         {
             string stopWord = "quit";
+
             while (stopWord != "exit")
             {
                 Console.WriteLine("Напишите стоп слово - exit");
                 stopWord = Convert.ToString(Console.ReadLine());
             }
+
         }
     }
 }
