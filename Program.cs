b68aa3cdece4a83cc28ce0db153f62e1204c783c
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-03 18:39:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,14 +4,26 @@ namespace JuniorHomeWork
     {
         static void Main(string[] args)
         {
-            int lowerNumber = 5;
-            int highestNumber = 96;
-            int step = 7;
+            Random rand = new Random();
+            int firstPositiveNumber = 3;
+            int secondPositiveNumber = 5;
+            int sumOfNumbers = 0;
+            int randomNumber = rand.Next(0, 100);
+
+            Console.WriteLine("Случайное число равно: " + randomNumber);
+            Console.ReadLine();
             
-            for (int i = lowerNumber; i <= highestNumber; i +=step)
+            for (int i = 0; i <= randomNumber; i++)
             {
-                Console.WriteLine(i);
+                if (i % firstPositiveNumber == 0 || i % secondPositiveNumber == 0)
+                {
+                    sumOfNumbers += i;
+                }
             }
+
+            Console.WriteLine("Сумма всех положительных чисел меньше " + randomNumber + " и кратных числам 3 и 5 равна: " 
+                + sumOfNumbers);
+            Console.ReadLine();
         }
     }
 }
