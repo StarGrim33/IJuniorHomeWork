862dd0084c487621240f7d63c5a6104cc0d6c899
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-06 09:32:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,9 +10,13 @@ namespace JuniorHomeWork
             string userInput;
             float currencyCount;
 
-            int rubToUsd = 68;
-            int rubToEur = 75;
+            float rubToUsd = 68.50f;
+            float rubToEur = 75.90f;
             float usdToEur = 0.95f;
+
+            float usdToRub = 59.58f;
+            float eurToRub = 66.70f;
+            float eurToUsd = 1.04f;
 
             Console.WriteLine("Приветствуем Вас в нашем обменнике валют, пожалуйста, введите количество рублей: ");
             rub = Convert.ToSingle(Console.ReadLine());
@@ -25,6 +29,9 @@ namespace JuniorHomeWork
             Console.WriteLine("1 - обмен рублей на доллары");
             Console.WriteLine("2 - обмен рублей на евро");
             Console.WriteLine("3 - обмен долларов на евро");
+            Console.WriteLine("4 - обмен долларов на рубли");
+            Console.WriteLine("5 - обмен евро на рубли");
+            Console.WriteLine("6 - обмен евро на доллары");
 
             userInput = Console.ReadLine();
 
@@ -37,7 +44,7 @@ namespace JuniorHomeWork
 
                     if (rub >= currencyCount)
                     {
-                        rub -= currencyCount;
+                        rub -= currencyCount; 
                         usd += currencyCount / rubToUsd;
                     }
                     else
@@ -70,11 +77,58 @@ namespace JuniorHomeWork
                     if (usd >= currencyCount)
                     {
                         usd -= currencyCount;
-                        usd -= currencyCount / usdToEur;
+                        eur += currencyCount / usdToEur;
                     }
                     else
                     {
                         Console.WriteLine("Вы ввели некорректное количество долларов");
+                    }
+                    break;
+
+                case "4":
+                    Console.WriteLine("Обмен долларов на рубли");
+                    Console.WriteLine("Сколько долларов Вы хотите обменять?");
+                    currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                    if (usd >= currencyCount)
+                    {
+                        usd -= currencyCount;
+                        rub += currencyCount * usdToRub;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели некорректное количество долларов");
+
+                    }
+                    break;
+                case "5":
+                    Console.WriteLine("Обмен евро на рубли");
+                    Console.WriteLine("Сколько евро Вы хотите обменять?");
+                    currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                    if (eur >= currencyCount)
+                    {
+                        eur -= currencyCount;
+                        rub += currencyCount * eurToRub;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели некорректное количество евро");
+                    }
+                    break;
+                case "6":
+                    Console.WriteLine("Обмен евро на доллары");
+                    Console.WriteLine("Сколько евро Вы хотите обменять");
+                    currencyCount = Convert.ToSingle(Console.ReadLine());
+
+                    if (eur >= currencyCount)
+                    {
+                        eur -= currencyCount;
+                        usd += currencyCount / eurToUsd;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы ввели некорректное количество евро");
                     }
                     break;
             }
