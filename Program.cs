0297bfda4e6db4ac6de287bacf6cde38ce670e50
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2022-07-08 22:19:03 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace JuniorHomeWork
+namespace JuniorHomeWork
 {
     internal class Program
     {
@@ -31,138 +31,242 @@
             Console.WriteLine("2 - Изменить пароль на вход в модули");
             Console.WriteLine("3 - Посмотреть количество ошибок серверов в модулях");
             Console.WriteLine("4 - Изменить цвет фона программы");
+            Console.WriteLine("5 - Выход из программы");
+
             userInput = Convert.ToString(Console.ReadLine());
 
-            switch (userInput)
+            bool done = false;
+            int esc;
+            while (!done)
             {
-                case "1":
-                    Console.Clear();
-                    Console.WriteLine("Общее количество серверных машин во всех модулях - " + sumOfServers);
-                    Console.WriteLine("Какой модуль Вас интересует ?");
-                    Console.WriteLine("1 - Альфа\n2 - Betta\n3 - Gamma");
-                    userChoice = Convert.ToInt32(Console.ReadLine());
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Общее количество серверных машин во всех модулях - " + sumOfServers);
+                        Console.WriteLine("Какой модуль Вас интересует ?");
+                        Console.WriteLine("1 - Альфа\n2 - Betta\n3 - Gamma");
+                        userChoice = Convert.ToInt32(Console.ReadLine());
 
-                    if (userChoice == 1)
-                    {
-                        Console.WriteLine("Количество серверных машин в модуле Альфа - " + moduleAlpha);
-                        Console.WriteLine("Чтобы вернуться назад нажмите - Esc");
-                    }
-                    else if (userChoice == 2)
-                    {
-                        Console.WriteLine("Количество серверных машин в модуле Бетта - " + moduleBetta);
-                    }
-                    else if (userChoice == 3)
-                    {
-                        Console.WriteLine("Количество серверных машин в модуле Бетта - " + moduleGamma);
-                    }
-                    break;
-
-                case "2":
-                    Console.WriteLine(" Установка пароля на вход в модули ");
-                    Console.WriteLine(" В каком модуле Вы хотите установить пароль на вход ? ");
-                    Console.WriteLine("1 - Альфа\n2 - Betta\n3 - Gamma");
-                    userChoice = Convert.ToInt32(Console.ReadLine());
-
-                    if (userChoice == 1)
-                    {
-                        Console.WriteLine("Введите текущий пароль: ");
-                        passwordAlpha = Convert.ToInt32(Console.ReadLine());
-                        if (passwordAlpha == 123456)
+                        if (userChoice == 1)
                         {
-                            Console.WriteLine("Введите новый пароль: ");
+                            Console.WriteLine("Количество серверных машин в модуле Альфа - " + moduleAlpha);
+                            Console.WriteLine("Нажмите 1 для выхода из программы или другую кнопку для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            }
+                        }
+                        if (userChoice == 2)
+                        {
+                            Console.WriteLine("Количество серверных машин в модуле Бетта - " + moduleBetta);
+                            Console.WriteLine("Нажмите 1 для выхода из программы или другую кнопку для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            }
+                        }
+                        if (userChoice == 3)
+                        {
+                            Console.WriteLine("Количество серверных машин в модуле Бетта - " + moduleGamma);
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        break;
+
+                    case "2":
+                        Console.WriteLine(" Установка пароля на вход в модули ");
+                        Console.WriteLine(" В каком модуле Вы хотите установить пароль на вход");
+                        Console.WriteLine("1 - Альфа\n2 - Betta\n3 - Gamma");
+                        userChoice = Convert.ToInt32(Console.ReadLine());
+
+                        if (userChoice == 1)
+                        {
+                            Console.WriteLine("Введите текущий пароль: ");
                             passwordAlpha = Convert.ToInt32(Console.ReadLine());
+                            if (passwordAlpha == 123456)
+                            {
+                                Console.WriteLine("Введите новый пароль: ");
+                                passwordAlpha = Convert.ToInt32(Console.ReadLine());
+                            }
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
                         }
-                        Console.WriteLine("Пароль изменен.");
-                    }
-                    else if (userChoice == 2)
-                    {
-                        Console.WriteLine("Введите текущий пароль: ");
-                        passwordBetta = Convert.ToInt32(Console.ReadLine());
-                        if (passwordBetta == 123456)
+                        else if (userChoice == 2)
                         {
-                            Console.WriteLine("Введите новый пароль: ");
+                            Console.WriteLine("Введите текущий пароль: ");
                             passwordBetta = Convert.ToInt32(Console.ReadLine());
+                            if (passwordBetta == 123456)
+                            {
+                                Console.WriteLine("Введите новый пароль: ");
+                                passwordBetta = Convert.ToInt32(Console.ReadLine());
+                            }
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
                         }
-                        Console.WriteLine("Пароль изменен.");
-                    }
-                    else if (userChoice == 3)
-                    {
-                        Console.WriteLine("Введите текущий пароль: ");
-                        passwordGamma = Convert.ToInt32(Console.ReadLine());
-                        if (passwordGamma == 123456)
+                        else if (userChoice == 3)
                         {
-                            Console.WriteLine("Введите новый пароль: ");
+                            Console.WriteLine("Введите текущий пароль: ");
                             passwordGamma = Convert.ToInt32(Console.ReadLine());
+                            if (passwordGamma == 123456)
+                            {
+                                Console.WriteLine("Введите новый пароль: ");
+                                passwordGamma = Convert.ToInt32(Console.ReadLine());
+                            }
+                            Console.WriteLine("Пароль изменен");
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
                         }
-                        Console.WriteLine("Пароль изменен");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Вы ввели некорректное значение");
-                    }
-                    break;
-                case "3":
-                    Console.WriteLine("Посмотреть количество ошибок серверов в модулях");
-                    Console.WriteLine("Какой модуль Вас интересует ?");
-                    Console.WriteLine("1 - Альфа\n2 - Betta\n3 - Gamma");
-                    userChoice = Convert.ToInt32(Console.ReadLine());
-                    if (userChoice == 1)
-                    {
-                        Console.WriteLine("Количество инцидентов в модуле Alpha - " + errorsInAlpha);
-                    }
-                    else if (userChoice == 2)
-                    {
-                        Console.WriteLine("Количество инцидентов в модуле Betta - " + errorsInBetta);
-                    }
-                    else if (userChoice == 3)
-                    {
-                        Console.WriteLine("Количество инцидентов в модуле Gamma - " + errorsInGamma);
-                    }
-                    break;
-                case "4":
-                    Console.WriteLine("Изменение цвета фона программы. Какой цвет Вы хотите выбрать: ? ");
-                    Console.WriteLine("1-Красный\n2-Голубой\n3-Зеленый\n4-Желтый\n5-Белый");
-                    userChoice= Convert.ToInt32(Console.ReadLine());
-                    if (userChoice == 1)
-                    {
+                        else
+                        {
+                            Console.WriteLine("Вы ввели некорректное значение");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Посмотреть количество ошибок серверов в модулях");
+                        Console.WriteLine("Какой модуль Вас интересует ?");
+                        Console.WriteLine("1 - Альфа\n2 - Betta\n3 - Gamma");
+                        userChoice = Convert.ToInt32(Console.ReadLine());
+                        if (userChoice == 1)
+                        {
+                            Console.WriteLine("Количество инцидентов в модуле Alpha - " + errorsInAlpha);
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        else if (userChoice == 2)
+                        {
+                            Console.WriteLine("Количество инцидентов в модуле Betta - " + errorsInBetta);
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        else if (userChoice == 3)
+                        {
+                            Console.WriteLine("Количество инцидентов в модуле Gamma - " + errorsInGamma);
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        break;
+                    case "4":
+                        Console.WriteLine("Изменение цвета фона программы. Какой цвет Вы хотите выбрать: ? ");
+                        Console.WriteLine("1-Красный\n2-Голубой\n3-Зеленый\n4-Желтый\n5-Белый");
+                        userChoice = Convert.ToInt32(Console.ReadLine());
+                        if (userChoice == 1)
+                        {
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Clear();
+                            Console.WriteLine("Цвет консоли изменен");
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        if (userChoice == 2)
+                        {
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.Blue;
+                            Console.Clear();
+                            Console.WriteLine("Цвет консоли изменен");
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        if (userChoice == 3)
+                        {
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.Green;
+                            Console.Clear();
+                            Console.WriteLine("Цвет консоли изменен");
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        if (userChoice == 4)
+                        {
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.Yellow;
+                            Console.Clear();
+                            Console.WriteLine("Цвет консоли изменен");
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        if (userChoice == 5)
+                        {
+                            Console.Clear();
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.Clear();
+                            Console.WriteLine("Цвет консоли изменен");
+                            Console.WriteLine("Пароль изменен.");
+                            Console.WriteLine("Нажмите 1 для выхода из программы или 2 для повторения");
+                            esc = Convert.ToInt32(Console.ReadLine());
+                            if (esc == 1)
+                            {
+                                done = true;
+                            };
+                        }
+                        break;
+                    case "5":
                         Console.Clear();
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.Clear();
-                        Console.WriteLine("Цвет консоли изменен");
-                    }
-                    if (userChoice == 2)
-                    {
-                        Console.Clear();
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                        Console.Clear();
-                        Console.WriteLine("Цвет консоли изменен");
-                    }
-                    if (userChoice == 3)
-                    {
-                        Console.Clear();
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.Clear();
-                        Console.WriteLine("Цвет консоли изменен");
-                    }
-                    if (userChoice == 4)
-                    {
-                        Console.Clear();
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.Clear();
-                        Console.WriteLine("Цвет консоли изменен");
-                    }
-                    if (userChoice == 5)
-                    {
-                        Console.Clear();
-                        Console.BackgroundColor = ConsoleColor.White;
-                        Console.Clear();
-                        Console.WriteLine("Цвет консоли изменен");
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Неизвестное значение ввода, пожалуйста, введите необходимую цифру для навигации по меню");
-                    break;
-            }
+                        done = true;
+                        break;
+                    default:
+                        Console.WriteLine("Неизвестное значение ввода, пожалуйста, введите необходимую цифру для навигации по меню");
+                        break;
+                }
             }
         }
     }
+}
